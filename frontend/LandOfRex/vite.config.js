55f21e044f0a8d6d036ad75604c044f3bd4679c7
박영훈 <pyh007264@gmail.com>
2025-11-25 16:53:21 +0900

    chore: roolback to check

--- frontend/LandOfRex/vite.config.js
+++ frontend/LandOfRex/vite.config.js
@@ -4,9 +4,9 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  build: {
-    outDir: "build",
-  },
+  // build: {
+  //   outDir: "build",
+  // },
   plugins: [react()],
   server: {
     // host: 'local.p.ssafy.io',
